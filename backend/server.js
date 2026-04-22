c1724f7d3fe36a5eb156aba996c6502f4556ce83
szymon <s00274674@atu.ie>
2026-04-22 18:33:43 +0100

    Added JWT middleware and verified protected routes

--- backend/server.js
+++ backend/server.js
@@ -16,6 +16,7 @@ app.use(express.json());
 
 // Routes
 app.use("/api/auth", require("./routes/authRoutes"));
+app.use("/api/test", require("./routes/testRoutes"));
 
 // Test route
 app.get("/", (req, res) => {
